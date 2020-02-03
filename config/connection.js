840982e5838820685f8520b236e8dc1e1972152b
Sergio Perez <perezsergio97.sp@gmail.com>
2020-02-02 21:25:19 -0600

    final commits

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,8 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
-  database: "burgers_db"
+  password: "Chicha27$",
+  database: "burgers2"
 });
 
 // Make connection.
