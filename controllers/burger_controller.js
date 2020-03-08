7022142f19cade114729bb233bb48078fe14a0ef
Sergio Perez <perezsergio97.sp@gmail.com>
2020-03-08 17:52:29 -0500

    fixed code for update burgers

--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -23,7 +23,7 @@ router.post("/api/burgers", function(req, res){
 
 router.put("/api/burgers/:id", function(req, res){
     var condition = "id= " + req.params.id
-    burgers.updated({
+    burgers.update({
         devoured: 1
     },
     condition, function(result){
